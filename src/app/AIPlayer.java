b92658b7b462916ba9afa587920cc886a6c01e9f
KozubIgn <ignacykozub@gmail.com>
2020-01-29 13:45:59 +0100

    create random shoot AIPlayer

--- src/app/AIPlayer.java
+++ src/app/AIPlayer.java
@@ -1,12 +1,15 @@
 package app;
 
-public class AIPlayer extends Player{
+import java.util.Random;
 
-    public String createShoot(){
-        String fromUser = "A1";
-        return "00";   // Random from PC
-    }
-       
+public class AIPlayer extends Player {
 
+     public int[] createShoot() {
+        int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Random random = new Random();
+        int randomNumberX = random.nextInt(numbers.length);
+        int randomNumberY = random.nextInt(numbers.length);
+        return new int[] {randomNumberX, randomNumberY};
+    }// Random from PC
 
 }
